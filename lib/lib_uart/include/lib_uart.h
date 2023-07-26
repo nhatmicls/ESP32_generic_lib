dd211ec866cbf0d3dff30e2b406ee8c6fd8e1608
Micls <cptprice123@outlook.com>
2023-07-26 19:19:47 +0700

    [Micls] Add uart init commit

--- lib/lib_uart/include/lib_uart.h
+++ lib/lib_uart/include/lib_uart.h
@@ -1,1 +1,38 @@
-void func(void);
+/**
+ * @file lib_uart.h
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-07-26
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef LIB_UART_H
+#define LIB_UART_H
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "esp_log.h"
+
+#define BUF_SIZE 1024
+
+struct uart_handle_t;
+
+struct uart_handle_t *lib_uart_init(uart_port_t uart_num,
+                                    uint16_t baud_rate,
+                                    uart_word_length_t data_bits,
+                                    uart_parity_t parity,
+                                    uart_stop_bits_t stop_bits,
+                                    uart_hw_flowcontrol_t flow_ctrl,
+                                    uint8_t txPort,
+                                    uint8_t rxPort,
+                                    uint8_t rtsPort,
+                                    uint8_t ctsPort);
+
+#endif
--- lib/lib_uart/lib_uart.c
+++ lib/lib_uart/lib_uart.c
@@ -1,6 +1,66 @@
 #include <stdio.h>
 #include "lib_uart.h"
 
-void func(void)
+struct uart_handle_t
 {
+    SemaphoreHandle_t xMutexUART;
+    QueueHandle_t uartQueue;
+    struct uart_handle_t *next_uart_handle;
+} __attribute__((packed));
+
+struct uart_handle_t *first_uart_handle = NULL;
+
+struct uart_handle_t *lib_uart_init(uart_port_t uart_num,
+                                    uint16_t baud_rate,
+                                    uart_word_length_t data_bits,
+                                    uart_parity_t parity,
+                                    uart_stop_bits_t stop_bits,
+                                    uart_hw_flowcontrol_t flow_ctrl,
+                                    uint8_t txPort,
+                                    uint8_t rxPort,
+                                    uint8_t rtsPort,
+                                    uint8_t ctsPort)
+{
+    static struct uart_handle_t *last_uart_handle_pointer = NULL;
+    struct uart_handle_t *uart_handle = (struct uart_handle_t *)malloc(sizeof(struct uart_handle_t));
+
+    if (last_uart_handle_pointer != NULL)
+    {
+        last_uart_handle_pointer->next_uart_handle = uart_handle;
+    }
+    else
+    {
+        first_uart_handle = uart_handle;
+    }
+
+    // create mutex
+    uart_handle->xMutexUART = xSemaphoreCreateMutex();
+    if (uart_handle->xMutexUART == NULL)
+    {
+        printf("Mutex can't allocated\n");
+        while (1)
+        {
+        }
+    }
+
+    uart_config_t uart_config = {
+        .baud_rate = baud_rate,
+        .data_bits = data_bits,
+        .parity = parity,
+        .stop_bits = stop_bits,
+        .flow_ctrl = flow_ctrl,
+        .rx_flow_ctrl_thresh = 122,
+    };
+
+    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
+
+    int intr_alloc_flags = 0;
+
+    ESP_ERROR_CHECK(uart_driver_install(uart_num, BUF_SIZE * 2, BUF_SIZE * 2, 20, &uart_handle->uartQueue, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(uart_num, txPort, rxPort, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+
+    last_uart_handle_pointer = uart_handle;
+
+    return uart_handle;
 }
